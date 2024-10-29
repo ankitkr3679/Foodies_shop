2308d318011ce2bbd7bab1bbeb64fffbc6eadbe3
ankitkr3679 <166656461+ankitkr3679@users.noreply.github.com>
2024-10-30 00:31:11 +0530

    footer

--- js/main.js
+++ js/main.js
@@ -34,8 +34,8 @@ navbar.forEach(function(a){
             }
         }, step);
     }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
-    counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
+    counter("count1", 0, 55, 3000);
+    counter("count2", 0, 85, 3000);
+    counter("count3", 0, 65, 3000);
+    counter("count4", 0, 95, 3000);
  });
